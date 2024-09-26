c3b834109b9f184cfc21ac46b69a2dc888529c43
4444ph <jnavarez20@gmail.com>
2024-09-27 00:56:06 +0800

    Updated

---
+++ lms-testing/app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,12 @@
+const SearchPage = () => {
+
+    return(
+
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Fugit, voluptatem incidunt et sint ipsam aut eligendi molestias cumque! Fugit at obcaecati odit, 
+            dicta unde alias accusamus debitis! Deleniti, dolores consequuntur.
+        </div>
+    )
+}
+
+export default SearchPage;
---
+++ lms-testing/app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -0,0 +1,14 @@
+const AnalyticsPage = () => {
+
+    return (
+
+        <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Voluptatem rem minima ex cum possimus sint sequi tempora aspernatur molestias corporis nihil, 
+            earum saepe quibusdam nulla odio dicta expedita inventore laudantium!
+        </div>
+
+    )
+}
+
+export default AnalyticsPage;
